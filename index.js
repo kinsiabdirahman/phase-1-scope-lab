fa4b48f5a2ec0fc6efcd0d3ce427ec7612731dd8
kinsiabdirahman <kinsiabdirahman@outlook.com>
2023-12-13 23:23:11 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+// Declaring customerName to be bob in global scope
+var customerName = "bob";
+
+// Modifying the customerName variable to be all uppercase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Setting the bestCustomer variable
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+// Overwriting the bestCustomer variable
+function overwriteBestCustomer(newFavorite) {
+  bestCustomer = newFavorite;
+}
+
+// Tryinging to reassign the leastFavoriteCustomer
+const leastFavoriteCustomer = "someone";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "someone else";
+}
